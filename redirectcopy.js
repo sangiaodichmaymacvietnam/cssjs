603339536d2171603d4fc525b28bc7f9893db2fb
sangiaodichmaymacvietnam <sangiaodichmaymac@gmail.com>
2016-07-28 19:32:44 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/> SaN GIAO DICH MAY MAC VIET NAM : <a href='http://www.sangiaodichmaymac.vn/'>http://www.sangiaodichmaymac.vn/"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> SAN GIAO DICH MAY MAC VIET NAM : <a href='http://www.sangiaodichmaymac.vn/'>http://www.sangiaodichmaymac.vn/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
